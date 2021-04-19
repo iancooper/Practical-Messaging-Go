ed5ed2a93c85633a8c713cc9ea855cecd8412e7b
Joseph Woodward <joseph.woodward@xeuse.com>
2021-04-19 14:52:07 +0100

    Add apostrophe

--- datatype-channel/cmd/greeting.go
+++ datatype-channel/cmd/greeting.go
@@ -5,13 +5,12 @@ import (
 	"time"
 )
 
-//We don't want to share code between sender and receiver, but its convenient for this exercise to do so
+//We don't want to share code between sender and receiver, but it's convenient for this exercise to do so
 
-type Greeting struct{
+type Greeting struct {
 	Message string
 }
 
-func (g Greeting) Greet(){
+func (g Greeting) Greet() {
 	log.Println("Received message: ", g.Message, "at", time.Now())
 }
-
